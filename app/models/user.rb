0ca039fabb87ee427edc20e55dc70fa550a28a72
da-ijb <ndeiwmj237@icloud.com>
2020-10-21 17:22:11 +0900

    モデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :furigana_last_name, presence: true
-  validates :furigana_first_name, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :furigana_last_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :furigana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
          
          
 end

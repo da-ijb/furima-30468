838a6772131a494f910e858d3debb483aef4a538
da-ijb <ndeiwmj237@icloud.com>
2020-10-26 17:45:55 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,24 +19,25 @@ class ItemsController < ApplicationController
   end 
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path
     end  
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit  
     end  
-  end   
+  end  
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end  
   
   private
   def item_params
